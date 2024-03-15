2a76849eeedf9b5f7ace8568459abd5ccb4f4964
SATHIYAMHURTHY <sathiya10102003@gmail.com>
2024-03-15 10:41:58 +0530

    third commit

--- src/Login.js
+++ src/Login.js
@@ -4,8 +4,9 @@ import Button from '@mui/material/Button'
 import "../src/Login.css"
 import { useFormik } from 'formik'
 import *as yup from "yup";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 export default function Login() {
+    const navigate = useNavigate()
     const loginValidationSchema = yup.object({
         email: yup.string().required().min(10).email(),
         password: yup.string().required().min(10),
@@ -27,7 +28,7 @@ export default function Login() {
         <h1>Login</h1>
         <TextField id="outlined-basic" label="E-mail" variant="outlined" value={formik.values.email} onChange={formik.handleChange} name="email" onBlur={formik.handleBlur} error={formik.touched.email && formik.errors.email} helperText={formik.touched.email && formik.errors.email ? formik.errors.email : null}/>
         <TextField id="outlined-basic" label="Password" variant="outlined" type='password' value={formik.values.trailer} onChange={formik.handleChange} name="password" onBlur={formik.handleBlur} error={formik.touched.password && formik.errors.password} helperText={formik.touched.password && formik.errors.password ? formik.errors.password : null}/>
-        <Button variant="contained" type='submit'>Login</Button>
+        <Button variant="contained" type='submit'  onClick={() => navigate(`/portal/home`)}>Login</Button>
         <h4>Don't have an account ? click Here <Link to="/register">Register</Link></h4>
     </form>
     </div>
--- src/Register.js
+++ src/Register.js
@@ -4,8 +4,9 @@ import Button from '@mui/material/Button'
 import "../src/Register.css"
 import { useFormik } from 'formik'
 import *as yup from "yup";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 export default function Register() {
+    const navigate = useNavigate()
     const registerValidationSchema = yup.object({
         name: yup.string().required(),
         email: yup.string().required().min(10).email(),
@@ -30,7 +31,7 @@ export default function Register() {
         <TextField id="outlined-basic" label="Name" variant="outlined" value={formik.values.name} onChange={formik.handleChange} name="name" onBlur={formik.handleBlur} error={formik.touched.name && formik.errors.name} helperText={formik.touched.name && formik.errors.name ? formik.errors.name : null}/>
         <TextField id="outlined-basic" label="E-mail" variant="outlined" value={formik.values.email} onChange={formik.handleChange} name="email" onBlur={formik.handleBlur} error={formik.touched.email && formik.errors.email} helperText={formik.touched.email && formik.errors.email ? formik.errors.email : null}/>
         <TextField id="outlined-basic" label="Password" variant="outlined" type='password' value={formik.values.trailer} onChange={formik.handleChange} name="password" onBlur={formik.handleBlur} error={formik.touched.password && formik.errors.password} helperText={formik.touched.password && formik.errors.password ? formik.errors.password : null}/>
-        <Button variant="contained" type='submit'>Register</Button>
+        <Button variant="contained" type='submit' onClick={() => navigate(`/`)} >Register</Button>
         <h4>Have an account ? click Here <Link to='/'>Login</Link></h4>
     </form>
     </div>
